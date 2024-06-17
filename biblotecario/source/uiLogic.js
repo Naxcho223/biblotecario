ab9848c926939ae916d1a2a89f42705cebae3c03
lukitasmois <lmoix85@gmail.com>
2024-06-17 19:35:21 -0300

    correccion de errores y funcion limpiar localstorage

--- biblotecario/source/uiLogic.js
+++ biblotecario/source/uiLogic.js
@@ -1,5 +1,5 @@
 document.querySelector('#miBoton').addEventListener('click',guardarUsuario)
-
+document.querySelector('#reset').addEventListener('click', resetUsers)
 showUsers()
 
 function guardarUsuario() {
@@ -10,12 +10,35 @@ function guardarUsuario() {
     const contraseña = document.getElementById('contraseña').value;
 
     addUser(nombre,apellido,email,telefono,contraseña)
+    showUsers()
 }
 
 function showUsers(){
-    let users = getUsers()
-    console.log(users)
-   // body = document.querySelector('#usuarios tbody')
+    let users = getUsers(),
+    body = document.querySelector('#usuarios tbody')
+
+    body.innerHTML = '';
+
+    for (let index = 0; index < users.length; index++) {
+        let row = body.insertRow(index),
+        nombreCell = row.insertCell(0),
+        apellidoCell = row.insertCell(1),
+        emailCell = row.insertCell(2),
+        telefonoCell = row.insertCell(3),
+        contraseñaCell = row.insertCell(4)
+
+        nombreCell.innerHTML = users[index].nombre;
+        apellidoCell.innerHTML = users[index].apellido;
+        emailCell.innerHTML = users[index].email;
+        telefonoCell.innerHTML = users[index].telefono;
+        contraseñaCell.innerHTML = users[index].contraseña;
+
+        body.appendChild(row);
+    }
 }
 
+function resetUsers(){
+    localStorage.clear();
+    showUsers()
+}
 
